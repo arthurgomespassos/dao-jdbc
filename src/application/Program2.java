780915f627e7a1926800258668d51acc86393a34
arthurgomespassos <arthurgomespassos74@gmail.com>
2022-07-01 18:17:00 -0300

    department findById

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,9 +1,18 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.DeparmentDao;
+import model.entities.Department;
+
 public class Program2 {
 
 	public static void main(String[] args) {		
+
+		DeparmentDao departmentDao = DaoFactory.createDepartmentDao();
 		
+		System.out.println("---- TEST 1: department findById ----");
+		Department department = departmentDao.findById(1);
+		System.out.println(department);
 	}
 
 }
